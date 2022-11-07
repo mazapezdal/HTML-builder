969c72eac2ca7db952cf828458b950b7fb52ed76
Vladislav Mazannik <mazanniknnik@gmail.com>
2022-11-08 01:35:08 +0300

    init: start/end 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,33 @@
+
+const fs = require('fs')
+const path = require('path');
+
+const text = path.join(__dirname, 'text.txt')
+const stream = fs.createReadStream(text,'utf-8');
+
+
+stream.on('data', 
+function(str){
+    console.log(String(str));
+});
+
+
+
+/*   'node 01-read-file'
+Работает, но это без ReadStream
+
+
+
+const fs = require('fs')
+const path = require('path');
+
+fs.readFile(
+    path.join(__dirname, 'text.txt'),
+    'utf-8',
+    (err, data) => {
+        if (err) throw err;
+        console.log(data);
+    }
+);
+
+*/
